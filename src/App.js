898d5c99e97fc5a0d40fcaf3b5001be4b1ec404b
Mauri Costa <mauri-m@hotmail.com>
2021-02-09 08:36:36 -0300

    Pagina de Login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {Container , Elements, Form, InputEmail, InputSenha, ButtonLog} from './style';
 
-function App() {
-  return (
-    <div>Hello World</div>
+function App(){
+
+  return(
+    <Container>
+      <Elements>
+        <Form>
+          <img src="https://energiasirius.com/wp-content/uploads/2020/04/solargroup-logo.png" />
+          <InputEmail />
+          <InputSenha />
+          <ButtonLog>Logar</ButtonLog>
+        </Form>
+      </Elements>
+    </Container>
   );
-}
+};
 
 export default App;
